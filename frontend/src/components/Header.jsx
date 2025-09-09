fa4a2a67695694bdb30c975c3dc60a6fef8e8225
kevin kalenga <kevinkalenga10@gmail.com>
2025-09-09 16:26:09 +0200

    Création de composant Header partie2

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -28,6 +28,44 @@ function Header() {
       }
 
 
+      return(
+        <header className="bg-primary text-white">
+             <div className="container mx-auto flex items-center just-between p-4">
+                 <Link to='/' className="flex items-center gap-2 text-lg font-bold">
+                    <img src="/images/logo.png" alt="logo" className="h-8" />
+                 </Link>
+                 <div className="md:hidden">
+                     <button onClick={() => setMenuOpen(!menuOpen)}>
+                         {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+                     </button>
+                 </div>
+                 <div className={`${menuOpen ? "block" : "hidden"}
+                  md:flex md:items-center md:gap-4 absolute
+                   md:relative top-16 md:top-0 w-full md:w-auto bg-primary md:bg-transparent z-50`}>
+                    <Link to='/cart' className="relative flex items-center gap-2 p-2 md:p-0">
+                        <FashoppingCart /> Panier
+                    </Link>
+
+                    {
+                        userInfo ? (
+                            <div className="relative">
+                                 <Link to="/profile" className="flex items-center gap-1 p-2 md:p-0 focus:outline-none">
+                                    <FaUser /> {userInfo.name}
+                                 </Link>
+                            </div>
+                        ): (
+                            <Link to='/login' className="flex items-center gap-1 p-2 md:p-0">
+                               <FaUser /> Connexion
+                            </Link>
+                        )
+                    }
+
+                 </div>
+             </div>
+        </header>
+      )
+
+
 
 }
 

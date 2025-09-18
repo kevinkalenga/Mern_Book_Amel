b3c7b4c73f175679dcb00b425e3c4489fa2f3d67
kevin kalenga <kevinkalenga10@gmail.com>
2025-09-18 15:45:45 +0200

    Product screen partie3

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -87,9 +87,77 @@ function ProductScreen() {
                              {product.countInStock > 0 ? "In Stock": "Out of stock"}
                          </span>
                        </p>
+
+                       {
+                        product.countInStock > 0 && (
+                            <div className='mt-4'>
+                               <label className='block text-gray-700 font-semibold' htmlFor="qty">Quantité</label>
+                               <select 
+                                className='w-full border rounded 
+                                focus:outline-none focus:ring-2
+                                 focus:ring-blue-500' value={qty} name="" id="qty"
+                                 onChange={(e) => setQty(Number(e.target.value))}
+                                 >
+                                   {
+                                     [...Array(product.countInStock).keys()].map((x) => {
+                                        <option key={x+1} value={x+1}>
+                                            {" "} {x + 1}
+                                        </option>
+                                     })
+                                   }
+                                   
+                               </select>
+                            </div>
+                        )
+                       }
+                       <button className={`w-full mt-6 py-3 rounded-lg font-semibold
+                                     ${product.countInStock === 0 ? "bg-gray-300 cursor-not-allowed" :
+                                      "bg-primary hover:bg-secondary text-white"}`}
+                                       disabled={product.countInStock===0}
+                                    >
+                                        Ajouter dans le panier
+                        </button>
                        
                      </div>
                    </div> 
+                   <div className='mt-8'>
+                      <h2 className='text-2xl font-bold text-gray-900'>Evaluations</h2>
+                      {
+                        product.review.length === 0 ? (
+                           <Message>
+                              Aucune evaluation trouvé
+                           </Message>
+                        ):(
+                           <div className='mt-6 space-y-5'>
+                               {
+                                 product.reviews.map((review) => (
+                                    <div className='bg-white p-6 rounded-lg shadow-sm' key={review._id}>
+                                       <div className='flex items-center space-x-4'>
+                                          <strong className='text-gray-900'>
+                                             {review.name}
+                                          </strong>
+                                          <Rating value={review.rating} />
+                                          <p className='text-sm text-gray-500'>
+                                              {review.createdAt.subString(0, 10)}
+                                          </p>
+
+                                       </div>
+                                       <p className='mt-2 text-gray-700'>{review.comment}</p>
+                                    </div>
+                                 ))
+                               }
+                           </div>
+                        )
+                      }
+                     {
+                        userInfo && isOpen && (
+                           <button className='w-48 py-2 bg-primary
+                            text-white rounded-lg font-semibold
+                             hover:bg-secondary mt-6' onClick={() => setIsOpen(true)}>Ajout d'une évaluation</button>
+                        )
+                     }
+
+                   </div>
 
                 </div>
             </div>

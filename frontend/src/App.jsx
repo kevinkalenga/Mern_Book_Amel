f721e45d629557dab65b6cc854eebd28a74f8ff2
kevin kalenga <kevinkalenga10@gmail.com>
2025-09-25 15:11:07 +0200

    L'implementation de la date d'expiration du token et react toastify frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,36 @@
 import Header from "./components/Header"
 import { Outlet } from "react-router-dom"
 import "react-toastify/dist/ReactToastify.css"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { logout } from "./slices/authSlice"
+import { ToastContainer } from "react-toastify"
 
 function App() {
-  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const checkTokenExpiration = () => {
+       try {
+        const expirationTime = localStorage.getItem("expirationTime")
+         if(!expirationTime) return 
+
+         const isExpired = Date.now() > Number(expirationTime)
+
+         if(isExpired) {
+          dispatch(logout())
+         }
+       } catch (error) {
+         console.log("L'erreur de verification de l'expiration du token", error)
+       }
+    }
+    checkTokenExpiration()
+  }, [dispatch])
 
   return (
     
       <>
+        <ToastContainer />
          <Header />
          <main>
            <Outlet />
